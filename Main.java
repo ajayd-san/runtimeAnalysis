73cb45d030f163f3a4b193df99730d8416b7a8a7
default-303 <ajayds2001@gmail.com>
2021-12-07 23:31:29 +0530

    added elem array

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@ import Utilities.Benchmark;
 public class Main {
     public static void main(String[] args) {
         Benchmark test = new Benchmark();
-
-        test.BenchmarkAll(1000000, 5);
+        int[] elems = {100, 1000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000};
+        for (int elem : elems)
+            test.BenchmarkAll(elem, 5);
     }
 }
